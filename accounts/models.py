1043616b0b22deb257a6988bdef9e2e1976af892
TAKANO <takanotomoo@gmail.com>
2024-07-30 14:45:47 +0900

    second

--- accounts/models.py
+++ accounts/models.py
@@ -113,7 +113,7 @@ class PostDaily(models.Model):
         return self.title
 
 
-# 仮作成
+# 仮作成 kesu 
 class To_do(models.Model):
     deadline = models.DateField(verbose_name="期日", blank=False, null=False, default=timezone.now)
     task = models.CharField(verbose_name="タスク名", blank=False, null=False, max_length=10)
